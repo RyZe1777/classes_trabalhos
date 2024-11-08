e273afb58dfda92e67b315196cb570a2f476db6d
Tony <145759988+RyZe1777@users.noreply.github.com>
2024-11-08 20:39:52 -0100

    Create

--- Veiculo.h
+++ Veiculo.h
@@ -1,18 +1,16 @@
 #ifndef VEICULO_H
 #define VEICULO_H
 
-
 #include <iostream>
-
 using namespace std;
 
 class Veiculo {
-    protected:
-        string vehicle_name;
+    protected: //protected pois pode ser acessada pelos "filhos"
+        string nome_veiculo;
 
     public:
-        Veiculo(string vehicle_name);
-        virtual void mostrarDetalhes();
+        Veiculo(string nome_veiculo);
+        virtual void MostrarDetalhes();  //virtual pois temos que dar overwrite neste metodo
 };
 
 #endif
