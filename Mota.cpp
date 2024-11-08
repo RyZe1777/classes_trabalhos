cbed617d5198aee5a90e76fcb25c420ae9736ca8
Tony <145759988+RyZe1777@users.noreply.github.com>
2024-11-08 20:48:59 -0100

    Create

---
+++ Mota.cpp
@@ -0,0 +1,7 @@
+#include "Mota.h"
+
+Mota::Mota(string nome, int cilindrada) : Veiculo(nome), cc(cilindrada) {}
+
+void Mota::MostrarDetalhes() {
+    cout << "O nome da Mota é: " << nome_veiculo << " / E esta tem: " << cc << " de cilindrada" << endl;
+}
