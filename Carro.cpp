fd1eaa368884dbc59ff12de14fe796dc1ed37b88
Tony <145759988+RyZe1777@users.noreply.github.com>
2024-11-13 20:47:12 -0100

    Update

--- Carro.cpp
+++ Carro.cpp
@@ -1,6 +1,6 @@
 #include "Carro.h"
 
-Carro::Carro(string nome , int portas) : Veiculo(nome), numero_de_portas(portas) {}
+Carro::Carro(string nome , int portas) : Veiculo(nome), numero_de_portas(portas) {} //vamos dizer oq de facto o construtor vai fazer
 
 void Carro::MostrarDetalhes() {
     cout << "O nome do Carro é: " << nome_veiculo << " / E este tem: " << numero_de_portas << " portas" << endl;
