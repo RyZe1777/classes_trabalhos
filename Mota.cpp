5910de8c880649f417a266ab8d7c2b86990c51e5
Tony <145759988+RyZe1777@users.noreply.github.com>
2024-11-14 18:23:17 -0100

    Update

--- Mota.cpp
+++ Mota.cpp
@@ -1,6 +1,6 @@
 #include "Mota.h"
 
-Mota::Mota(string nome, int cilindrada) : Veiculo(nome), cc(cilindrada) {} //vamos dizer oq de facto o construtor vai fazer
+Mota::Mota(string nome, int cilindrada) : Veiculo(nome), cc(cilindrada) {} 
 
 void Mota::MostrarDetalhes() {
     cout << "O nome da Mota é: " << nome_veiculo << " / E esta tem: " << cc << " de cilindrada" << endl;
